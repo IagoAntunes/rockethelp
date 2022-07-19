d846058e6576aa01229e26669f6bd48d5152cade
IagoAntunes <iagoantunes.f@gmail.com>
2022-07-18 22:49:56 -0300

    rockethelp

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,12 +1,44 @@
-import {VStack, Heading} from 'native-base';
+//Estados
+import {useState} from 'react';
 
+//Bibliotecas
+import {VStack, Heading, Icon,useTheme} from 'native-base';
+import { color } from 'native-base/lib/typescript/theme/styled-system';
+import {Envelope, Key} from 'phosphor-react-native';
+
+//Imagem
+import Logo from '../assets/logo_primary.svg';
+
+//Componentes
+import {Input} from '../components/Input';
+import {Button} from '../components/Button';
 
 export function SignIn(){
+    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
+
+    const {colors} = useTheme();
     return(
         <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
+            <Logo/>
             <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
                 Acesse sua conta
             </Heading>
+
+            <Input 
+            mb={4}
+            placeholder="E-mail"
+            InputLeftElement={<Icon as ={<Envelope color={colors.gray[300]}/>}/>}ml={4}
+            onChangeText={setName}
+            />
+            <Input 
+            mb={8}
+            placeholder="Senha"
+            InputLeftElement={<Icon as ={<Key color={colors.gray[300]}/>}/>}ml={4}
+            secureTextEntry
+            onChangeText={setPassword}
+            />
+            <Button title='Entrar' w="full"/>
         </VStack>
     )
 }
